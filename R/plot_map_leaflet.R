1443fe9d06dda01fe5822cd0331f26ea9e01cc68
Lindh Markus <a002293@ad.smhi.se>
2020-11-07 08:34:28 +0200

    update plot_map_leaflet

--- R/plot_map_leaflet.R
+++ R/plot_map_leaflet.R
@@ -7,23 +7,9 @@
 #'   many elements as there are rows, by default the row names are shown.
 #' @return HTML widget object.
 #' @export
-plot_map_leaflet <- function(data, provider = "Esri.OceanBasemap", popup = NULL) {
-  check_lonlat(data, FALSE)
-  if (!is.null(popup) && all(popup %in% names(data))) {
-    if(length(popup) == 1) {
-      popupdata <- as.character(data[,popup])
-    } else {
-      popupdata <- apply(data[,popup], 1, knitr::kable, format="html")
-      popupdata <- gsub("<thead>.*</thead>", "", popupdata) # remove column header
-    }
-  } else if (length(popup) == NROW(data)) {
-    popupdata <- as.character(popup)
-  } else {
-    popupdata <- as.character(rownames(data))
-  }
-
+plot_map_leaflet <- function(data, provider = "Esri.OceanBasemap") {
   m <- leaflet(data) %>%
     addProviderTiles(provider) %>%
-    addCircleMarkers(~sample_longitude_dd, ~sample_latitude_dd, popup = popupdata, radius = 3, weight = 1, fillColor = "#FF368B", color = "#FF368B", opacity = 1, fillOpacity = 0.1)
+    addCircleMarkers(~sample_longitude_dd, ~sample_latitude_dd, popup = ~station_name, radius = 3, weight = 1, fillColor = "#FF368B", color = "#FF368B", opacity = 1, fillOpacity = 0.1)
   return(m)
 }
