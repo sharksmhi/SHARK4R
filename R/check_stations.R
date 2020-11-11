ef8be1173be69457af6d04657444829b9a89a989
Lindh Markus <a002293@ad.smhi.se>
2020-11-11 22:13:48 +0200

    added new function check_stations()

---
+++ R/check_stations.R
@@ -0,0 +1,53 @@
+#' Station matching using SMHI station list "stations.txt" (the list is synced with "Stationsregistret": https://stationsregister.miljodatasamverkan.se/stationsregister/composer/)
+#' matches reported station name in data with curated station list
+#' @param names Vector of station names.
+#' @return Data frame with station name and match type.
+#' @export
+
+match_station <- function(names) {
+  
+  station_db <- read_delim(system.file("extdata", "stations.txt", package = "SHARK4R"), delim ="\t", guess_max = 2000, col_names = T, locale = readr::locale(encoding = "latin1"))
+  
+  match_index <- match(names, station_db$STATION_NAME)
+  
+  match_type <- names %in% station_db$STATION_NAME
+  
+  matches <- data.frame(reported_station_name = names, match_type = match_type)
+  
+  if (length(which(match_type == FALSE))>0) {
+    message("WARNING: Unmatched stations found, check synonyms")
+    print(matches[!match_type,])
+  }
+  else {
+    message("All stations found")
+  }
+}
+
+#' Station distance check using SMHI station list "stations.txt" (the list is synced with "Stationsregistret": https://stationsregister.miljodatasamverkan.se/stationsregister/composer/)
+#' matches reported station name in data with curated station list and checks if it is within preset distance
+#' @param names Vector of station names.
+#' @param latitude and longitude
+#' @return Data frame with station name and logical value within/outside preset distance limits.
+#' @export
+
+check_station_distance<- function(names, lat, lon) {
+  
+  station_db <- read_delim(system.file("extdata", "stations.txt", package = "SHARK4R"), delim ="\t", guess_max = 2000, col_names = T, locale = readr::locale(encoding = "latin1"))
+  
+  match_index <- match(names, station_db$STATION_NAME)
+  
+  match_type <- names %in% station_db$STATION_NAME
+  
+  matches <- data.frame(reported_station_name = names, match_type = match_type)
+  
+  if (length(which(match_type == FALSE))>0) {
+    message("WARNING: Unmatched stations found, check synonyms")
+    print(matches[!match_type,])
+  }
+  else if (length(which(match_type == TRUE))>0) {
+    message("WARNING: Matched stations found but they are outside distance limit")
+  }
+  else {
+    message("All stations found within distance limits")
+  }
+}
