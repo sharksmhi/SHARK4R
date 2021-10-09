43b9bb6b8169b6316cb593a50c1e250e5b7c0aa8
Lindh Markus <a002293@ad.smhi.se>
2021-10-09 09:05:48 +0200

    Incorporate Dyntaxa match taxa into match_taxa function

--- R/match_taxa.R
+++ R/match_taxa.R
@@ -1,3 +1,33 @@
+#' Taxon matching using Dyntaxa (https://www.dyntaxa.se/)
+#' matches latin name in data with Dyntaxa taxon list
+#' @param names Vector of scientific names.
+#' @param ask Ask user in case of multiple matches.
+#' @return Data frame with scientific name, scientific name ID and match type.
+#' @export
+
+match_dyntaxa <- function(names) {
+  
+  f <- as.factor(names)
+  indices <- as.numeric(f)
+  unames <- levels(f)
+  
+  dyntaxa_db <- read_delim(system.file("extdata", "dyntaxa_Biota.txt", package = "SHARK4R"), delim ="\t", guess_max = 2000, col_names = T, locale = readr::locale(encoding = "latin1"))
+  
+  match_index <- match(names, dyntaxa_db$ScientificName)
+  
+  match_type <- names %in% dyntaxa_db$ScientificName
+  
+  matches <- data.frame(reported_ScientificName = names, match_type = match_type)
+  
+  if (length(which(match_type == FALSE))>0) {
+    message("Unmatched taxa found")
+    print(matches[!match_type,])
+  }
+  else {
+    message("All taxa found")
+  }
+}
+
 #' Taxon matching using WoRMS (http://www.marinespecies.org/)
 #' matches latin name in data with WoRMS taxon list
 #' @param names Vector of scientific names.
