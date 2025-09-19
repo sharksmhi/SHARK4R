14379ed3ffbffb17c3cc52ed5b34c153e45e93ed
anderstorstensson <anders.torstensson@smhi.se>
2025-09-19 14:06:40 +0200

    add tests

--- R/match_taxa.R
+++ R/match_taxa.R
@@ -4,7 +4,6 @@
 #' @param ask Ask user in case of multiple matches.
 #' @return Data frame with scientific name, scientific name ID and match type.
 #' @export
-
 match_wormstaxa <- function(names, ask = TRUE) {
 
   f <- as.factor(names)
@@ -12,10 +11,10 @@ match_wormstaxa <- function(names, ask = TRUE) {
   unames <- levels(f)
 
   pages <- split(unames, as.integer((seq_along(unames) - 1) / 50))
-  paged_worms_taxamatch_call <- function(page) { cache_call(page, expression(worrms::wm_records_taxamatch(page)))}
+  paged_worms_taxamatch_call <- function(page) { cache_call(page, expression(worrms::wm_records_taxamatch(page, marine_only = FALSE)))}
   matches <- unlist(lapply(pages, paged_worms_taxamatch_call), recursive = FALSE)
 
-  results <- data.frame(scientificName = character(), match_type = character(), stringsAsFactors = FALSE)
+  results <- data.frame(scientificName = character(), scientificNameID = character(), match_type = character(), acceptedNameUsageID = character(), stringsAsFactors = FALSE)
 
   # count no matches and multiple matches
 
@@ -54,29 +53,33 @@ match_wormstaxa <- function(names, ask = TRUE) {
 
   for (i in seq_along(matches)) {
 
-    row <- list(scientificName = NA, match_type = NA)
+    row <- list(scientificName = NA, scientificNameID = NA, match_type = NA, acceptedNameUsageID = NA)
 
     match <- matches[[i]]
-    if (is.data.frame(match)) {
+    if (is.data.frame(match) & nrow(match) > 0) {
 
       if (nrow(match) == 1) {
 
         # single match
 
         row$scientificName = match$scientificname
+        row$scientificNameID = match$lsid
         row$match_type = match$match_type
+        row$acceptedNameUsageID = as.character(match$valid_AphiaID)
 
       } else if (ask) {
 
         # multiple matches
 
-        print(match %>% select(scientificname, authority, status, match_type))
+        print(match %>% select(AphiaID, scientificname, authority, status, match_type))
         message(unames[i])
         n <- readline(prompt = "Multiple matches, pick a number or leave empty to skip: ")
         s <- as.integer(n)
         if (!is.na(n) & n > 0 & n <= nrow(match)) {
           row$scientificName = match$scientificname[s]
+          row$scientificNameID = match$lsid[s]
           row$match_type = match$match_type[s]
+          row$acceptedNameUsageID = as.character(match$valid_AphiaID[s])
         }
 
       }
---
+++ tests/testthat/test-scatterplot.R
@@ -0,0 +1,36 @@
+test_that("scatterplot returns a valid plotly object", {
+  df <- data.frame(
+    station_name = c("Stn1", "Stn2"),
+    value = c(10, 20),
+    sample_date = as.Date(c("2020-01-01", "2020-01-02"))
+  )
+
+  p <- scatterplot(df)
+
+  # Class check
+  expect_s3_class(p, "plotly")
+  expect_s3_class(p, "htmlwidget")
+
+  # Get the unique plot ID
+  id <- p$x$cur_data[[1]]
+
+  # Check trace attributes
+  trace <- p$x$attrs[[id]]
+  expect_equal(trace$type, "scatter")
+  expect_equal(trace$mode, "markers")
+  expect_equal(as.character(trace$x)[2], "station_name")
+  expect_equal(as.character(trace$y)[2], "value")
+  expect_true(trace$visible)
+
+  # Check layout updatemenus
+  um <- p$x$layoutAttrs[[id]]$updatemenus
+  expect_equal(length(um), 2)
+
+  # Y-axis menu
+  expect_equal(um[[1]]$buttons[[1]]$label, "Y = VALUE")
+  expect_equal(um[[1]]$buttons[[1]]$args[[1]], "y")
+
+  # X-axis menu
+  expect_equal(um[[2]]$buttons[[1]]$label, "X = STATION_NAME")
+  expect_equal(um[[2]]$buttons[[2]]$label, "X = SAMPLE_DATE")
+})
